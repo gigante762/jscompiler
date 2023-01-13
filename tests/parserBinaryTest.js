f87850a87ae34141eb6f4cca81bd3b9147e0fbb7
Kevin Souza <kevin.souza@devsquad.com>
2023-01-12 21:24:28 -0300

    format

--- tests/parserBinaryTest.js
+++ tests/parserBinaryTest.js
@@ -2,7 +2,7 @@ import { Parser } from "../Parser.js";
 
 let parser = new Parser();
 
-const program = parser.parse("10 + 2 * 5 ");
+const program = parser.parse("10 + 2 * 5");
 
 console.log(program);
 //enhance this test
